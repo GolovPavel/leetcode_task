79245b7df2624ca43fcb7606e4177fc72902e145
GolovPavel <golovpasha@gmail.com>
2021-04-25 21:35:33 +0300

    Container with most water problem solved

---
+++ src/main/kotlin/ru/golov/leetcode/tasks/medium/find_all_anagrams_in_string/Solution.java
@@ -0,0 +1,21 @@
+package ru.golov.leetcode.tasks.medium.find_all_anagrams_in_string;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+class Solution {
+
+    /**
+     * s = "cbaebabacd", p = "abc"
+     * left = 0, 0, 0
+     * right = 0, 1, 2
+     * cArr = [3, 3, 2, 1], [3, 3, 1, 1], [3, 2, 1, 1]
+     * count = 3, 2, 1
+     *
+     * result = []
+     */
+
+
+}
