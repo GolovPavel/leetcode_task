ce56da6d49f1480ba58aa75385ef17fa269ed589
GolovPavel <golovpasha@gmail.com>
2021-04-28 14:07:07 +0300

    Find all anagrams in string problem solved

--- src/main/kotlin/ru/golov/leetcode/tasks/medium/find_all_anagrams_in_string/Solution.java
+++ src/main/kotlin/ru/golov/leetcode/tasks/medium/find_all_anagrams_in_string/Solution.java
@@ -1,21 +1,50 @@
 package ru.golov.leetcode.tasks.medium.find_all_anagrams_in_string;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
+import java.util.ArrayList;
 
 class Solution {
 
-    /**
-     * s = "cbaebabacd", p = "abc"
-     * left = 0, 0, 0
-     * right = 0, 1, 2
-     * cArr = [3, 3, 2, 1], [3, 3, 1, 1], [3, 2, 1, 1]
-     * count = 3, 2, 1
-     *
-     * result = []
-     */
+    // f("a", "a") -> [0]
+    // f("ab", "a") -> [0]
+    // f("ba", "a") -> [1]
+    // f("abccba", "abc") -> [0, 3]
+    // f("abba", "ab") -> [0, 2]
+    // f("cbaebabacd", "abc") -> [0, 6]
+    public List<Integer> findAnagrams(String s, String p) {
+        var count = new int[26];
+        var result = new ArrayList<Integer>();
 
+        for (int i = 0; i < p.length(); i++) {
+            count[p.charAt(i) - 'a']++;
+        }
 
+        var start = 0;
+
+        for (int end = 0; end < s.length(); end++) {
+            count[s.charAt(end) - 'a']--;
+
+            if (end - start + 1 < p.length()) {
+                continue;
+            }
+
+            var isAnagram = true;
+
+            for (int c : count) {
+                if (c != 0) {
+                    isAnagram = false;
+                    break;
+                }
+            }
+
+            if (isAnagram) {
+                result.add(start);
+            }
+
+            count[s.charAt(start) - 'a']++;
+            start++;
+        }
+
+        return result;
+    }
 }
